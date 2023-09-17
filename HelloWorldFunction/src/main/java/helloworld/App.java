6622dd8ca1c625a73713a13160515233c04b69a6
joseong-ug <hoho88@naver.com>
2023-09-18 00:19:51 +0900

    init commit

---
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -0,0 +1,58 @@
+package helloworld;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.Test;
+
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+        Map<String, String> queryStringMap = input.getQueryStringParameters();
+        String body = input.getBody();
+        String version = input.getVersion();
+        try {
+            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
+
+            JSONObject obj1 = new JSONObject();
+            obj1.put("pageContents", pageContents);
+
+            return response
+                    .withStatusCode(200)
+                    .withBody(obj1.toJSONString());
+        } catch (Exception e) {
+            return response
+                    .withBody("{}")
+                    .withStatusCode(500);
+        }
+    }
+
+    private String getPageContents(String address) throws IOException{
+        URL url = new URL(address);
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            return br.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+}
---
+++ HelloWorldFunction/src/test/java/helloworld/AppTest.java
@@ -0,0 +1,36 @@
+package helloworld;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppTest {
+  @Test
+  public void successfulResponse() {
+    App app = new App();
+    APIGatewayProxyRequestEvent input = new APIGatewayProxyRequestEvent();
+    Map<String, String> map = new HashMap<>();
+    map.put("test", "1234");
+    input.setQueryStringParameters(map);
+    String body = String.format("{ \"code\": \"1111\", \"message\": \"hello\" }");
+    input.setBody(body);
+
+    APIGatewayProxyResponseEvent result = app.handleRequest(input, null);
+    assertEquals(200, result.getStatusCode().intValue());
+    assertEquals("application/json", result.getHeaders().get("Content-Type"));
+    String content = result.getBody();
+
+    System.out.println("시스템로그 []: content : "+ content);
+
+    assertNotNull(content);
+    assertTrue(content.contains("pageContent"));
+    //assertTrue(content.contains("\"hello world\""));
+    //assertTrue(content.contains("\"location\""));
+  }
+}
