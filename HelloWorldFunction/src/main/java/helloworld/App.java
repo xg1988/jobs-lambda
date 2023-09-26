f71b9c9098cf5f303282f7774dc091ca8e4755b5
joseong-ug <hoho88@naver.com>
2023-09-27 06:21:45 +0900

    - put 개발완료 이제 get

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -17,9 +17,7 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
-import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
+import com.amazonaws.services.dynamodbv2.model.*;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
@@ -50,28 +48,36 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         String body             = input.getBody();
         String version          = input.getVersion();
 
-        System.out.println("시스템로그 [body]: " + body);
-        System.out.println("시스템로그 [version]: " + version);
-        System.out.println("input -> "+ input);
+        System.out.println("시스템로그 [getFunctionName]: " + context.getFunctionName());
+        System.out.println("시스템로그 [getAwsRequestId]: " + context.getAwsRequestId());
+        System.out.println("시스템로그 [getFunctionVersion]: " + context.getFunctionVersion());
+
+        System.out.println("시스템로그 [getHttpMethod]: " + input.getHttpMethod());
+        System.out.println("시스템로그 [getBody]: " + input.getBody());
+        System.out.println("시스템로그 [getResource]: " + input.getResource());
+        System.out.println("시스템로그 [getPath]: " + input.getPath());
+
+        Object result = null;
+
         try {
             //final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            AmazonDynamoDB ddb = _connectDynamoDB();
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            Comment comment = objectMapper.readValue(body, Comment.class);
 
             try {
-                Map<String, AttributeValue> item_values = new HashMap<>();
-                item_values.put("guid",             new AttributeValue(UUID.randomUUID().toString()));
-                item_values.put("registDate",       new AttributeValue(String.valueOf(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddhhmm")))));
-                item_values.put("comment",          new AttributeValue(comment.getComment()));
-                item_values.put("userId",           new AttributeValue(comment.getUserId()));
-                item_values.put("type",           new AttributeValue(comment.getType()));
+                switch (input.getHttpMethod()){
+                    case "GET":
+                        result = get(body);
+                        break;
+                    case "POST":
 
-                System.out.println("시스템로그 [item_values]: " + item_values.toString());
+                        break;
+                    case "DELETE":
 
-                ddb.putItem("Comment", item_values);
+                        break;
+                    case "PUT":
+                        result = put(body);
+                        break;
+                }
+
 
             } catch (ResourceNotFoundException e) {
                 System.out.println("시스템로그 []: " + e);
@@ -82,13 +88,43 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
 
             return response
                     .withStatusCode(200)
-                    .withBody(comment.toString());
+                    .withBody(body);
         } catch (Exception e) {
             System.out.println("시스템로그 []: e : "+ e);
             return response
                     .withBody("{}")
                     .withStatusCode(500);
         }
+    }
+
+    private List<Comment> get(String body){
+        AmazonDynamoDB ddb = _connectDynamoDB();
+        GetItemResult getItemResult = ddb.getItem("Comment", null);
+        System.out.println("시스템로그 []: " + getItemResult.toString());
+
+
+        return null;
+    }
+
+    private Comment put(String body) throws JsonProcessingException {
+        AmazonDynamoDB ddb = _connectDynamoDB();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        Comment comment = objectMapper.readValue(body, Comment.class);
+
+        Map<String, AttributeValue> item_values = new HashMap<>();
+        item_values.put("guid",             new AttributeValue(UUID.randomUUID().toString()));
+        item_values.put("registDate",       new AttributeValue(String.valueOf(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddhhmm")))));
+        item_values.put("comment",          new AttributeValue(comment.getComment()));
+        item_values.put("userId",           new AttributeValue(comment.getUserId()));
+        item_values.put("type",             new AttributeValue(comment.getType()));
+
+        System.out.println("시스템로그 [item_values]: " + item_values.toString());
+
+        ddb.putItem("Comment", item_values);
+
+        return comment;
     }
 
     private AmazonDynamoDB _connectDynamoDB() {
--- HelloWorldFunction/src/test/java/helloworld/AppTest.java
+++ HelloWorldFunction/src/test/java/helloworld/AppTest.java
@@ -13,23 +13,6 @@ import java.util.Map;
 public class AppTest {
   @Test
   public void successfulResponse() {
-    App app = new App();
-    APIGatewayProxyRequestEvent input = new APIGatewayProxyRequestEvent();
-    Map<String, String> map = new HashMap<>();
-    map.put("test", "1234");
-    input.setQueryStringParameters(map);
-    String body = String.format("{ \"userId\": \"xg1988\", \"comment\": \"hello\", \"type\": \"01\" }");
-    input.setBody(body);
 
-    APIGatewayProxyResponseEvent result = app.handleRequest(input, null);
-    assertEquals(200, result.getStatusCode().intValue());
-    assertEquals("application/json", result.getHeaders().get("Content-Type"));
-    String content = result.getBody();
-
-    System.out.println("시스템로그 [content]  : "+ content);
-
-    //assertNotNull(content);
-    //assertTrue(content.contains("\"hello world\""));
-    //assertTrue(content.contains("\"location\""));
   }
 }
